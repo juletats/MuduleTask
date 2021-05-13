cbffcd5275242c70af62af9690afa9cf6b0e4dcb
19188728 <DArTsaturyan@sberbank.ru>
2021-05-13 10:05:23 +0300

    Third Commit

--- src/main/java/FileParsing.java
+++ src/main/java/FileParsing.java
@@ -4,19 +4,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public  class FileParsing {
-     public static List<City> read(String path, List<City>cities) {
+     public static Scanner scanner;
+    public static List<City> read(String path, List<City>cities) {
         try {
-            Scanner scanner = new Scanner(new File(path));
+          scanner= new Scanner(new File(path));
             int i = 0;
             while (scanner.hasNextLine()) {
                 String s = scanner.nextLine();
                 String[] str = s.split(";");
                 cities.add(new City(str[1], str[2], str[3], Integer.parseInt(str[4]), Integer.parseInt(str[5])));
                 i++;
+
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+         scanner.close();
         return cities;
     }
 }
